99ff220834944be739cc0839fa4e30f56cfb60ff
Mike Hatch <mike.s.hatch@gmail.com>
2019-02-11 16:45:17 -0600

    Added menu list with a jquery ajax call

---
+++ dist/jquery.js
@@ -0,0 +1,16 @@
+ // jQuery ajax function (Menu)
+ $(function() {
+  $('.load-info').on('click', function() {
+    $('.load-info').hide()
+    $.ajax({
+      method: 'GET',
+      url: 'data_menu.json',
+      dataType: 'json'
+    }).done(function(data) {
+      $.each(data.hamburgers, function(key, val) {
+        $('.info-menu')
+          .append('<li>' + val.name + '</li>')
+      }) // end each
+    }) // end ajax
+  }) // end click event
+}) // end jQuery ready
